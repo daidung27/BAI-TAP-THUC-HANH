b5d1cec5b190156fe93681e01b49ad80ac7549de
daidung27 <drakecheems@gmail.com>
2025-12-10 08:42:11 +0700

    Add files via upload

---
+++ bai3/cau3_7.py
@@ -0,0 +1,8 @@
+print("Tong doan dai dung")
+print("MSV: 24575202161009")
+def checkValue(n):  
+  if n%2 == 0:  
+   print ("Đây là một số chẵn")  
+  else:  
+   print ("Đây là một số lẻ")
+checkValue(6) 
