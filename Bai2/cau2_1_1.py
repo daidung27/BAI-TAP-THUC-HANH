36252130df0eedb9499a667d8dc6f3a816625d8d
daidung27 <drakecheems@gmail.com>
2025-12-10 08:40:58 +0700

    Add files via upload

---
+++ Bai2/cau2_1_1.py
@@ -0,0 +1,8 @@
+print("sinh vien: Tong Doan Dai Dung")
+print("msv:245752021610009")
+n1=int(input("enter n1 value"))
+n2=int(inpit("enter n2 value"))
+if n1>n2:
+    print("n1 is big")
+else:
+    print("n2 is big")
